03e0903886f9df0cd92253aa6e883ee3e677104d
Erich McAlister <65683159+eamcalister@users.noreply.github.com>
2025-09-02 22:30:13 +0000

    Latest build deployed.

--- worksheets/lunr-pretext-search-index.js
+++ worksheets/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Front Matter",
   "number": "",
   "title": "Front Matter",
-  "body": "  Mathematics Department Fort Lewis College  Last Updated:       "
+  "body": "  Mathematics Department Fort Lewis College       "
 },
 {
   "id": "sec-section-name-4",
@@ -62,6 +62,60 @@ var ptx_lunr_docs = [
   "number": "4",
   "title": "",
   "body": "  Find formulas for quadratic functions satisfying each description below.     The -intercept is at and there are two -intercepts at and .      The vertex is at and the graph passes through the point .    "
+},
+{
+  "id": "sec-section-name-5",
+  "level": "1",
+  "url": "sec-section-name-5.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 121 Week 2 Activity",
+  "body": " Math 121 Week 2 Activity   In this activity we will do the following:    Show how all linear functions may be built as transformations of .    Explore how all quadratic functions may be built as transformations of .    Provide an alternative way of putting a quadratic function into vertex form based on graphical transformations.      This activity is intended to help you review some of the algebraic functions you saw in previous math courses. Please feel free to work together. If you get stuck, just ask and we'll get unstuck.  Please note, some of these questions have answers that will require you to write complete sentences in order to help you make better sense of the math you're learning. This may seem difficult at first.      Consider the problem: find a linear function with slope whose -intercept is at .     Find a formula for and sketch its graph.      What transformations were applied to the function to obtain ?      Now suppose that instead of having a -intercept at , we want a function whose graph passes through the point . What additional transformation needs to be applied to your basic function to get this new function? Find the formula for this transformation and graph it. How else have we described this formula for a linear function?         In this exercise, we will describe the vertex form as a sequence of transformations applied to .     Find the formula for a quadratic function whose vertex is at and whose graph passes through the point .      What transformations are applied (give them in the correct order too) to to obtain the function you found in part (a)?      Consider the general quadratic function in vertex form Describe the sequence of transformations, in the correct order, that you use to create from the parent function .         Here's another way to find the vertex of a quadratic using simpler functions and shifts. Consider the function We can write this as a vertical shift of the function upward by units. Notice that This means (from which came) has easily identified zeros at and . It's vertex lies halfway between them at . Then we can compute the -value of this vertex to be Thus, the vertex of is units higher at . So the vertex form of is      Use the idea from the example to put the function into vertex form.     "
+},
+{
+  "id": "sec-section-name-5-2",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": " In this activity we will do the following:    Show how all linear functions may be built as transformations of .    Explore how all quadratic functions may be built as transformations of .    Provide an alternative way of putting a quadratic function into vertex form based on graphical transformations.    "
+},
+{
+  "id": "sec-section-name-5-4-1",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-4-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Consider the problem: find a linear function with slope whose -intercept is at .     Find a formula for and sketch its graph.      What transformations were applied to the function to obtain ?      Now suppose that instead of having a -intercept at , we want a function whose graph passes through the point . What additional transformation needs to be applied to your basic function to get this new function? Find the formula for this transformation and graph it. How else have we described this formula for a linear function?    "
+},
+{
+  "id": "sec-section-name-5-5-1",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  In this exercise, we will describe the vertex form as a sequence of transformations applied to .     Find the formula for a quadratic function whose vertex is at and whose graph passes through the point .      What transformations are applied (give them in the correct order too) to to obtain the function you found in part (a)?      Consider the general quadratic function in vertex form Describe the sequence of transformations, in the correct order, that you use to create from the parent function .     "
+},
+{
+  "id": "sec-section-name-5-6-1",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-6-1",
+  "type": "Example",
+  "number": "1",
+  "title": "",
+  "body": " Here's another way to find the vertex of a quadratic using simpler functions and shifts. Consider the function We can write this as a vertical shift of the function upward by units. Notice that This means (from which came) has easily identified zeros at and . It's vertex lies halfway between them at . Then we can compute the -value of this vertex to be Thus, the vertex of is units higher at . So the vertex form of is   "
+},
+{
+  "id": "sec-section-name-5-6-2",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-6-2",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Use the idea from the example to put the function into vertex form.   "
 }
 ]
 
